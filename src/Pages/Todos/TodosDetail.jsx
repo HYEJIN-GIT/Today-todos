9ac5381fa0a0cb457b8f1ccbd27d080c856fd296
HYEJIN KWON <chahaa0189@gmail.com>
2026-05-10 15:49:09 +0900

    feat : zustand localstoage 기능 추가

--- src/Pages/Todos/TodosDetail.jsx
+++ src/Pages/Todos/TodosDetail.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { replace, useNavigate, useParams } from 'react-router-dom'
 import { todosStore } from '../../Store/todoStore'
 
 const TodosDetail = () => {
   const {id} = useParams()
-  console.log(id)
-  const {todoList} = todosStore()
- 
+  const {todoList, deleteTodos,completeTodos} = todosStore()
+  const navigate = useNavigate()
+  
   const detailList = todoList?.find((item)=>
     item.id ===  Number(id))
-  
+  const deleteList = (id)=>{
+    deleteTodos(id)
+    navigate('/todos', replace)
+  }
+  const editList = (content)=>{
+    console.log(content)
+  }
   
     return (
     <div>
-    <div>{detailList?.title}</div>
-    <div>{detailList?.content}</div>
+    <div>제목 : {detailList?.title}</div>
+    <div>오늘의 할일을 자세히 알려주세요 ! {detailList?.content}</div>
+    <button className="btn" onClick={()=>deleteList(detailList?.id)}>삭제</button>
+    <button className='btn' onClick={()=>editList(deleteList?.content)}>수정</button>
       
     </div>
   )
--- src/Store/todoStore.js
+++ src/Store/todoStore.js
@@ -1,24 +1,27 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const todosStore = create((set)=>({
-
-    todoList: [],
-    addTodos : (todo,content)=>set((state=>({
-        todoList : [...state.todoList, {
-            id: Date.now(),
-            title : todo ,
-            content,
-            isDone : false
-        }]
-    }))),
-    deleteTodos:(id)=>set((state)=>({
-        todoList: state.todoList.filter((item)=>item.id !== id)
-    })),
-    completeTodos : (id)=>set((state)=>({
-        todoList : state.todoList.map((item)=>{
-        return   item.id === id ? {...item,isDone:!item.isDone} : item
-        })
-    })),
-
-
-}))
+export const todosStore = create( 
+    persist((set)=>({
+        todoList: [],
+            addTodos : (todo,content)=>set((state=>({
+                todoList : [...state.todoList, {
+                    id: Date.now(),
+                    title : todo ,
+                    content,
+                    isDone : false
+                }]
+            }))),
+            deleteTodos:(id)=>set((state)=>({
+                todoList: state.todoList.filter((item)=>item.id !== id)
+            })),
+            completeTodos : (id)=>set((state)=>({
+                todoList : state.todoList.map((item)=>{
+                return   item.id === id ? {...item,isDone:!item.isDone} : item
+                })
+            })),
+        
+    }),{
+        name : "todo-storage"
+    })   
+)
